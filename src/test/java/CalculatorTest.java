16de1d1d0174f220dadfeb36c724c5a45b6e6804
ankursinghpatel <ankur.patel@siemens.com>
2025-06-12 10:06:25 +0000

    Add first unit test but build not working

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class CalculatorTest {
+
+  Calculator calculator = new Calculator();
+
+  @Test
+  public void testSum() {
+    int expected = 3;
+
+    int actual = calculator.sum(1, 2);
+
+    assertEquals(expected, actual);
+  }
+
+}
