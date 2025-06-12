0fea547e354659ad3bd7a088ec0af03f532d472e
ankursinghpatel <ankur.patel@siemens.com>
2025-06-12 11:24:42 +0000

    module 3.3 end

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,6 +1,6 @@
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class CalculatorTest {
 
@@ -53,6 +53,43 @@ public class CalculatorTest {
     
     int actual = calculator.divide(18, 6);
     
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testIsEven() {
+
+    boolean actual = calculator.isEven(2);
+
+    assertTrue(actual);
+  }
+
+  @Test
+  public void testIsOdd() {
+
+    boolean actual = calculator.isEven(3);
+
+    assertFalse(actual);
+  }
+
+  @Test
+  public void testIncrementArray() {
+    int[] expected = new int[]{2, 3, 4};
+
+    int[] actual = calculator.incrementArray(new int[]{1, 2, 3});
+
+    assertArrayEquals(expected, actual);
+  }
+
+  @Test
+  public void testDivideByZero() {
+    
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> calculator.divide(1, 0));
+
+    String expected = "Ints cannot be divided by zero";
+
+    String actual = exception.getMessage();
+
     assertEquals(expected, actual);
   }
 }
