c37899c128eb93581063c6e2d8cb454eb039d59f
ankursinghpatel <ankur.patel@siemens.com>
2025-06-12 10:48:31 +0000

    module 2 challenge end

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,25 @@
 public class Calculator {
 
-  public int sum(int x, int y) {
+  // public int sum(int x, int y) {
+  //   return x + y;
+  // }
+
+  public int add(int x, int y) {
     return x + y;
+  }
+
+  //This method should return the result of subtracting y from x
+  public int subtract(int x, int y) {
+    return x - y;
+  }
+
+  //This method should return the result of multiplying x and y.
+  public int multiply (int x, int y) {
+    return x * y;
+  }
+
+  //This method should return the result of dividing x by y.
+  public int divide(int x, int y) {
+    return x / y;
   }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,23 +6,53 @@ public class CalculatorTest {
 
   Calculator calculator = new Calculator();
   
+  // @Test
+  // public void testSum() {
+    
+  //   int expected = 4;
+    
+  //   int actual = calculator.sum(1, 2);
+    
+  //   assertNotEquals(expected, actual);
+  // }
+
   @Test
-  public void testSum() {
+  public void testAdd() {
     
-    int expected = 4;
+    int expected = 3;
     
-    int actual = calculator.sum(1, 2);
+    int actual = calculator.add(1, 2);
     
-    assertNotEquals(expected, actual);
+    assertEquals(expected, actual);
   }
 
   @Test
-  public void testSum2() {
+  public void testSubtract() {
+    
+    int expected = 8;
+    
+    int actual = calculator.subtract(10, 2);
+    
+    assertEquals(expected, actual);
+  }
 
-    int expected = 5;
+  @Test
+  public void testMultiply() {
+    
+    int expected = 18;
+    
+    int actual = calculator.multiply(6, 3);
+    
+    assertEquals(expected, actual);
+  }
 
-    int actual = calculator.sum(2, 3);
-
+  @Test
+  public void testDivide() {
+    
+    int expected = 3;
+    
+    int actual = calculator.divide(18, 6);
+    
     assertEquals(expected, actual);
   }
 }
